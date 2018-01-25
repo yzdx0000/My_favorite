5059359feadb78e21b8772227ce68fce89e307da
yzdx0000 <1059079664@qq.com>
2018-01-25 21:11:57 +0800

    update

--- Word_Cloud/world_cloud.py
+++ Word_Cloud/world_cloud.py
@@ -5,18 +5,29 @@
 from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
 from scipy.misc import imread
+import sys
+import os
 
 # 读取一个txt文件
+tools = os.path.dirname(os.getcwd()) + r"\Word_Cloud\tools"  # 获取附件绝对路径
+jpg = r"%s\xinxing.jpg" % tools  # 背景图片
+txt = r'C:\Users\wx\Desktop\test.txt'
 
-text = open(r'C:\Users\Wind.wang\Desktop\test.txt', 'rb').read().decode('utf-8')
+if os.path.exists(txt):
+    text = open(txt, 'rb').read().decode('utf-8')
+
+else:
+    print("txt文件不存在")
+    sys.exit()  # 不存在txt则退出程序，不再执行以后的代码,此处用quit()也可以
 
 # 读入背景图片
 
-bg_pic = imread(r'C:\Users\Wind.wang\Desktop\3.jpg')
+bg_pic = imread(jpg)
 
 # 生成词云
 
-wordcloud = WordCloud(mask=bg_pic,  font_path=r"C:\Windows\Fonts\simhei.ttf",background_color='white', scale=1.5).generate(text)
+wordcloud = WordCloud(mask=bg_pic, font_path=r"C:\Windows\Fonts\simhei.ttf", background_color='white',
+                      scale=1.5).generate(text)
 
 image_colors = ImageColorGenerator(bg_pic)
 # 显示词云图片
